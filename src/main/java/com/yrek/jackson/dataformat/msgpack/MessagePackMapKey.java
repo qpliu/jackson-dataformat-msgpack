feffe5c1790f74b2ae59a981400197533068f1f9
Q.P.Liu <qpliu@yahoo.com>
2013-08-12 14:23:44 -0700

    @JacksonAnnotation is unneeded.

--- src/main/java/com/yrek/jackson/dataformat/msgpack/MessagePackMapKey.java
+++ src/main/java/com/yrek/jackson/dataformat/msgpack/MessagePackMapKey.java
@@ -5,14 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.fasterxml.jackson.annotation.JacksonAnnotation;
-
 /**
  * Mark a field to serialize and deserialize the key as an integer.
  */
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
-@JacksonAnnotation
 public @interface MessagePackMapKey {
     int value();
 }
